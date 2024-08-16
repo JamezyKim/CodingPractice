07820db831cdf35430fb59fe0ac2433db1eaf502
James Kim <yt4921@gmail.com>
2024-08-15 18:43:48 -0700

    fix binary search function

--- Search.c
+++ Search.c
@@ -7,33 +7,20 @@ void selectionSort(int* userArray, int sizeOfArray);
 int binarySearch(int* userArray, int target, int sizeOfArray);
 int insertionSort(int* userArray, int sizeOfArray);
 void swap(int* userArray, int firstIndex, int secondIndex);
+void printArray(int* userArray, int sizeOfArray);
 
 
 int main() {
-	int userArray[4] = { 4,7,3,1 };
-	int target;
+	int userArray[7] = { 4,7,6,10,9,3,1 };
+	int target = 3;
 	char userChoice[100];
 	int counter = 0;
 	int sizeOfArray = sizeof(userArray)/sizeof(userArray[0]);
 	char userOption[2];
+	int min = 0;
+	int max = sizeOfArray - 1;
 
-	//printf("Please choose your target. The array is { 4,7,3,1 }: \n");
-	//scanf_s("%s", userChoice, (unsigned)_countof(userChoice));
-
-	target = atoi(userChoice);
-
-
-	/*selectionSort(userArray, sizeOfArray);*/
-
-	//binarySearch(userArray, target, sizeOfArray);
-	for (int i = 0; i < 4; i++) {
-		printf("%d ", userArray[i]);
-	}
-	printf("\n");
-	swap(userArray, 0, 3);
-	for (int i = 0; i < 4; i++) {
-		printf("%d ", userArray[i]);
-	}
+	binarySearch(userArray, target, sizeOfArray, max, min);
 
 	return 0;
 }
@@ -52,55 +39,46 @@ int linearSearch(int* userArray,int target, int sizeOfArray) {
 
 int binarySearch(int* userArray, int target, int sizeOfArray) {
 	
-	int lowNum = 0;
-	int highNum = sizeOfArray-1;
-	int midPoint = lowNum + (highNum - lowNum) / 2;
+	int min = 0;
+	int max = sizeOfArray-1;
+	int midPoint = (max+min) / 2;
 	int resultIndex;
-	//userArray[4] = { 1,3,4,7 };
+	printf("Original array: ");
+	printArray(userArray, sizeOfArray);
+	selectionSort(userArray, sizeOfArray);
 	
-	while (lowNum <= highNum) {
-		if (userArray[midPoint] == target) {
-			resultIndex = userArray;
-			printf("%d", resultIndex);
-		}
-		else if (userArray[midPoint] < target) {
-			lowNum = midPoint + 1;
-		}
-		else if (userArray[midPoint] > target) {
-			highNum = midPoint - 1;
-		}
-
+	if (userArray[midPoint] == target) {
+		printf("\nSorted array: ");
+		printArray(userArray, sizeOfArray);
+		return midPoint;
+		
 	}
-	return resultIndex;
+	else if (userArray[midPoint] < target) {
+		min = midPoint + 1;
+		return binarySearch(userArray, target, sizeOfArray, max, min);
+	}
+	else if (userArray[midPoint] > target) {
+		max = midPoint - 1;
+		return binarySearch(userArray, target, sizeOfArray, max, min);
+	}
 
 }
 
 void selectionSort(int* userArray, int sizeOfArray) {
 
-	int temp;
-	int minIndex;
-	int* sortedArray[4] = { 0 };
-	
-	for (int i = 0; i < sizeOfArray - 1; i++) {
-		minIndex = i;
+	int minIndex = 0;
 
+	for (int i = 0; i < sizeOfArray; i++) {
+		minIndex = i;
 		for (int j = i + 1; j < sizeOfArray; j++) {
-			if (userArray[minIndex]>userArray[j]) {
-				minIndex = j;				
+			if (userArray[minIndex] != userArray[j]) {
+				if (userArray[minIndex] > userArray[j]) {
+					minIndex = j;
+				}
 			}
 		}
-
-		if (minIndex != i) {
-			temp = userArray[i];
-			userArray[i] = userArray[minIndex];
-			userArray[minIndex] = temp;
-		}
+		swap(userArray, minIndex, i);
 	}
-	printf("Sorted array: ");
-	for (int i = 0; i < sizeOfArray; i++) {
-		sortedArray[i] = userArray[i];
-	}
-	printf("\n");
 }
 
 int insertionSort(int* userArray, int sizeOfArray) {
@@ -122,4 +100,12 @@ void swap(int* userArray,int firstIndex,int secondIndex) {
 	userArray[secondIndex] = temp;
 
 	return;
+}
+
+void printArray(int* userArray, int sizeOfArray) {
+	for (int i = 0; i < sizeOfArray; i++) {
+		printf("%d ", userArray[i]);
+	}
+	printf("\n");
+
 }
